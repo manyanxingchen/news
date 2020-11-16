65fe063a63c9eff68ed952590d53ab20bfb1c6ae
manyanxingchen <1125419301@qq.com>
2020-11-16 20:35:48 +0800

    抽取config配置信息

---
+++ config.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+from redis import StrictRedis
+
+class Config(object):
+    #flask应用程序调试
+    DEBUG = True
+    #session加密配置
+    SECRET_KEY = 'JALSJDFLKAJSLK'
+    #数据库配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/news'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #redis配置信息
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = '6379'
+    #session配置信息
+    SESSION_TYPE = 'redis'  #设定session的存储类型
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)  #指定session存储的redis服务器
+    SESSION_USE_SIGNER = True  #设置签名存储
+    PERMANENT_SESSION_LIFETIME = timedelta(days=2)  #设置session有效期  秒使用seconds
+#开发环境配置信息
+class DevelopConfig(Config):
+    pass
+
+#生产（线上）环境配置信息
+class ProductConfig(Config):
+    DEBUG = False
+#测试环境配置信息
+class TestConfig(Config):
+    pass
+
+#给外界提供一个统一的访问接口
+config_dict={
+    'develop':'DevelopConfig',
+    'product':'ProductConfig',
+    'test':'TestConfig'
+}
--- manager.py
+++ manager.py
@@ -5,25 +5,11 @@ from redis import StrictRedis
 from flask_session import Session
 from datetime import timedelta
 from flask_wtf.csrf import CSRFProtect
-#创建flask实例化程序
+from config import Config
+
 app = Flask(__name__)
 #创建配置型系类
-class Config(object):
-    #flask应用程序调试
-    DEBUG = True
-    #session加密配置
-    SECRET_KEY = 'JALSJDFLKAJSLK'
-    #数据库配置信息
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/news'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #redis配置信息
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = '6379'
-    #session配置信息
-    SESSION_TYPE = 'redis'  #设定session的存储类型
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)  #指定session存储的redis服务器
-    SESSION_USE_SIGNER = True  #设置签名存储
-    PERMANENT_SESSION_LIFETIME = timedelta(days=2)  #设置session有效期  秒使用seconds
+
 #把配置信息加载到app中
 app.config.from_object(Config)
 #创建数据库应用实例化程序
