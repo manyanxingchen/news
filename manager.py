eaf973e14096cd95551e891fa53b857ac7418261
manyanxingchen <1125419301@qq.com>
2020-11-16 19:34:48 +0800

    添加了CSRFProtect保护

--- manager.py
+++ manager.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_session import Session
 from datetime import timedelta
+from flask_wtf.csrf import CSRFProtect
 #创建flask实例化程序
 app = Flask(__name__)
 #创建配置型系类
@@ -31,8 +32,10 @@ db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
 #创建session实例化程序，读取app中的session配置信息
 Session(app)
+#使用CSRFProtect保护app    ['POST', 'PUT', 'PATCH', 'DELETE']  保护这四种请求方式
+CSRFProtect(app)
 #定义路由
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 #定义视图函数
 def index():
     redis_store.set("name","zhangsan")
