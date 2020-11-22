99d24f71ab29ed7dd2c69249d84d78e7dcd66776
manyanxingchen <1125419301@qq.com>
2020-11-22 21:44:19 +0800

    分类加载更多数据

--- new/modules/index/view.py
+++ new/modules/index/view.py
@@ -1,14 +1,61 @@
 #from new.modules.index import index_blue
+from sqlalchemy import text
+
 from . import index_blue  #点表示的是当前的路径下
-from ... import redis_store
-import logging
-from flask import current_app, render_template, session, jsonify
+from flask import current_app, render_template, session, jsonify, request
 
 #定义蓝图路由
 from ...models import User, News, Category
 from ...utils.response_code import RET
+#首页分页显示新闻列表
+# 请求路径  /newlist
+# 请求方式  get
+# 请求参数  分类   第几页   每一页显示的对象
+# 返回值  errno errmsg
+@index_blue.route('/newslist',methods=['GET'])
+def newlist():
+    #1.获取参数
+    cid = request.args.get('cid','1')
+    page = request.args.get('page','1')
+    per_page = request.args.get('per_page','10')
+    #2.转换参数格式（获取的参数类型为字符型，分页查询中的函数需要整型参数）
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        page = 1
+        per_page = 10
 
-
+    #3.分页查询
+    try:
+        if cid == '1':
+            paginate = News.query.filter().order_by(News.create_time.desc()).paginate(page,per_page,False)
+        else:
+            paginate = News.query.filter(News.category_id==cid).order_by(News.create_time.desc()).paginate(page,per_page,False)
+        # """
+        #方式2.
+        # filters = text(" ")
+        # if cid != "1":
+        #     filters = (News.category_id == cid)
+        # paginate = News.query.filter(filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        # 方式3.
+        # filters = []
+        # if cid != "1":
+        #     filters.append(News.category_id == cid)
+        # paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno = RET.DBERR,errmsg = '分页新闻获取失败')
+    #4.获取到分页对象的属性:当前页，总页数，当前页的对象列表
+    total_pages = paginate.pages
+    current_page = paginate.page
+    items = paginate.items
+    #5.将对象列表转换为字典列表
+    news_list = []
+    for item in items:
+        news_list.append(item.to_dict())
+    #6.携带数据，返回响应
+    return jsonify(errno = RET.OK,errmsg = '分页信息获取成功',totalPage=total_pages,currentPage=current_page,newsList=news_list)
 @index_blue.route('/',methods=['GET','POST'])
 #定义视图函数
 def index():
