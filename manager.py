a44ac76826b5f0758a5615e37d553108b1bec587
manyanxingchen <1125419301@qq.com>
2020-12-26 21:30:34 +0800

    用户图表绘制

--- manager.py
+++ manager.py
@@ -1,4 +1,7 @@
 #导入扩展程序
+from datetime import datetime,timedelta
+from random import randint
+
 from flask import current_app
 
 from new import create_app,db,models  #导入models是让整个程序知道有models存在
@@ -34,6 +37,26 @@ def create_superuser(username,password):
         current_app.logger.error(e)
         return '创建失败'
     return '创建成功'
+#为了图表好看批量创建用户
+@manager.option('-t', '--test', dest='test')
+def test(test):
+    #批量添加用户
+    user_list = []
+    for i in range(0,1000):
+        user = User()
+        user.nick_name = '张三%s'%i
+        user.mobile = '139%08d'%i
+        user.password_hash = 'pbkdf2:sha256:150000$AWOXUM5P$8cfe321f09633bee119f904d15a1a7372824257b8236874fe6907e4086f2d3d2'
+        user.last_login = datetime.now() - timedelta(seconds=randint(0,3600*24*31))
+        user_list.append(user)
+    #批量添加到数据库
+    try:
+        db.session.add_all(user_list)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return '添加测试用户失败'
+    return '添加测试用户成功'
 #运行程序
 if __name__ =='__main__':
     manager.run()
--- new/modules/admin/views.py
+++ new/modules/admin/views.py
@@ -1,11 +1,64 @@
 #导入蓝图
-from flask import render_template, request, current_app, session, redirect, g
+from datetime import datetime
 
+from flask import render_template, request, current_app, session, redirect, g
+import time
 from . import admin_blue
 from ...models import User
 from ...utils.commons import user_login_data
+from datetime import datetime,timedelta
+@admin_blue.route('/user_count')
+def user_count():
+    try:
+        total_count = User.query.filter(User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error
+        return render_template('admin1/user_count.html',errmsg = '总人数查询失败')
+    try:
+        #1.先获取本月1日0点的日期时间
+        localtime = time.localtime()
+        #2.获取本月日期字符串
+        month_start_time_str = '%s-%s-01'%(localtime.tm_year,localtime.tm_mon)
+        #3.将字符串转换为日期类型
+        month_start_time_date = datetime.strptime(month_start_time_str,'%Y-%m-%d')
 
-
+        month_count = User.query.filter(User.last_login>=month_start_time_date,User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error
+        return render_template('admin1/user_count.html',errmsg = '获取月活人数失败')
+    try:
+        #1.先获取本月1日0点的日期时间
+        day_start_time_str = '%s-%s-%s'%(localtime.tm_year,localtime.tm_mon,localtime.tm_mday)
+        day_start_time_date = datetime.strptime(day_start_time_str,'%Y-%m-%d')
+        day_count = User.query.filter(User.last_login>=day_start_time_date,User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error
+        return render_template('admin1/user_count.html',errmsg = '获取日活人数失败')
+    #获取活跃时间段内，对应的活跃人数
+    active_date = []  #获取活跃日期
+    active_count = []  #获取活跃人数
+    for i in range(0,31):
+        #当前开始时间A
+        begin_date = day_start_time_date - timedelta(days=i)
+        #当天开始时间的后一天B
+        end_date = day_start_time_date - timedelta(days=i-1)
+        #添加当天开始时间字符串到，活跃日期中
+        active_date.append(begin_date.strftime('%m-%d'))
+        #查询时间A到B这一天的注册人数
+        every_active_count = User.query.filter(User.is_admin==False,User.last_login>=begin_date,User.last_login<=end_date).count()
+        #添加当天注册人数到，获取数量中
+        active_count.append(every_active_count)
+    #为了图表显示方便
+    active_date.reverse()
+    active_count.reverse()
+    data = {
+        'total_count':total_count,
+        'month_count':month_count,
+        'day_count':day_count,
+        'active_count':active_count,
+        'active_date':active_date
+    }
+    return render_template('admin1/user_count.html',data = data)
 @admin_blue.route('/index')
 @user_login_data
 def index():
